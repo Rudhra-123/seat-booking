a92784829ca49206308405051ffa3f73895d22fa
Hemant Kumar Jayas <hemantjayas03@gmail.com>
2023-07-19 13:04:35 +0530

    mongodb String

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const connect = () => {
     try {
         console.log("database Connected")
-        return mongoose.connect(process.env.MONGODB_URI)
+        return mongoose.connect(`${process.env.MONGODB_URI}`)
 
     } catch (error) {
         console.log(`databasee connection failed due to ${error.message}`)
